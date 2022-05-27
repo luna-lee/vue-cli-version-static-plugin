91a3d29008a26d5168115692d3a6ee75ff14d541
lip_mac <1724956493@qq.com>
2022-05-27 09:05:28 +0800

    修复css 独立打包且在相对路径下对静态资源的引用的bug

--- dist/AddCssSataticDir.js
+++ dist/AddCssSataticDir.js
@@ -1,128 +1,107 @@
-const cssnano = require("cssnano");
-const postcss = require("postcss");
-const isProd =
-  process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production";
+const cssnano = require('cssnano');
+const postcss = require('postcss');
+const isProd = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production';
 try {
-  // vuecli4.0
-  const OptimizeCssnanoPlugin = require("@intervolga/optimize-cssnano-plugin");
+    // vuecli4.0
+    const OptimizeCssnanoPlugin = require('@intervolga/optimize-cssnano-plugin');
 
-  if (OptimizeCssnanoPlugin)
-    // 修改OptimizeCssnanoPlugin的prototype
-    OptimizeCssnanoPlugin.prototype.apply = function (compiler) {
-      const self = this;
-      compiler.hooks.emit.tapAsync(
-        "OptimizeCssnanoPlugin",
-        function (compilation, callback) {
-          // Search for CSS assets
-          const assetsNames = Object.keys(compilation.assets).filter(
-            (assetName) => {
-              return /\.css$/i.test(assetName);
-            }
-          );
+    if (OptimizeCssnanoPlugin)
+        // 修改OptimizeCssnanoPlugin的prototype
+        OptimizeCssnanoPlugin.prototype.apply = function (compiler) {
+            const self = this;
+            compiler.hooks.emit.tapAsync('OptimizeCssnanoPlugin', function (compilation, callback) {
+                // Search for CSS assets
+                const assetsNames = Object.keys(compilation.assets).filter((assetName) => {
+                    return /\.css$/i.test(assetName);
+                });
 
-          let hasErrors = false;
-          const promises = [];
-          // Generate promises for each minification
-          assetsNames.forEach((assetName) => {
-            // Original CSS
-            const asset = compilation.assets[assetName];
-            const originalCss = asset.source();
+                let hasErrors = false;
+                const promises = [];
+                // Generate promises for each minification
+                assetsNames.forEach((assetName) => {
+                    // Original CSS
+                    const asset = compilation.assets[assetName];
+                    const originalCss = asset.source();
 
-            // Options for particalar cssnano call
-            const postCssOptions = {
-              from: assetName,
-              to: assetName,
-              map: false,
-            };
-            const cssnanoOptions = self.options.cssnanoOptions;
+                    // Options for particalar cssnano call
+                    const postCssOptions = {
+                        from: assetName,
+                        to: assetName,
+                        map: false
+                    };
+                    const cssnanoOptions = self.options.cssnanoOptions;
 
-            // Extract or remove previous map
-            const mapName = assetName + ".map";
-            if (self.options.sourceMap) {
-              // Use previous map if exist...
-              if (compilation.assets[mapName]) {
-                const mapObject = JSON.parse(
-                  compilation.assets[mapName].source()
-                );
+                    // Extract or remove previous map
+                    const mapName = assetName + '.map';
+                    if (self.options.sourceMap) {
+                        // Use previous map if exist...
+                        if (compilation.assets[mapName]) {
+                            const mapObject = JSON.parse(compilation.assets[mapName].source());
 
-                // ... and not empty
-                if (
-                  mapObject.sources.length > 0 ||
-                  mapObject.mappings.length > 0
-                ) {
-                  postCssOptions.map = Object.assign(
-                    {
-                      prev: compilation.assets[mapName].source(),
-                    },
-                    self.options.sourceMap
-                  );
-                } else {
-                  postCssOptions.map = Object.assign(
-                    {},
-                    self.options.sourceMap
-                  );
-                }
-              }
-            } else {
-              delete compilation.assets[mapName];
-            }
+                            // ... and not empty
+                            if (mapObject.sources.length > 0 || mapObject.mappings.length > 0) {
+                                postCssOptions.map = Object.assign(
+                                    {
+                                        prev: compilation.assets[mapName].source()
+                                    },
+                                    self.options.sourceMap
+                                );
+                            } else {
+                                postCssOptions.map = Object.assign({}, self.options.sourceMap);
+                            }
+                        }
+                    } else {
+                        delete compilation.assets[mapName];
+                    }
 
-            // Run minification
-            const promise = postcss([cssnano(cssnanoOptions)])
-              .process(originalCss, postCssOptions)
-              .then((result) => {
-                if (hasErrors) {
-                  return;
-                }
-                /* 对所有css中，绝对路径引用静态文件路径前面加上 publicPath和版本 */
-                // Extract CSS back to assets
-                let reg = new RegExp(process.env.RegExpStr, "g");
-                const processedCss = isProd
-                  ? result.css.replace(reg, process.env.JsCssStaticReplaceDir)
-                  : result.css;
-                compilation.assets[assetName] = {
-                  source: function () {
-                    return processedCss;
-                  },
-                  size: function () {
-                    return processedCss.length;
-                  },
-                };
+                    // Run minification
+                    const promise = postcss([cssnano(cssnanoOptions)])
+                        .process(originalCss, postCssOptions)
+                        .then((result) => {
+                            if (hasErrors) {
+                                return;
+                            }
+                            /* 对所有css中，绝对路径引用静态文件路径前面加上 publicPath和版本 */
+                            // Extract CSS back to assets
+                            let reg = new RegExp(process.env.RegExpStr, 'g');
+                            const processedCss = isProd ? result.css.replace(reg, process.env.CssStaticReplaceDir) : result.css;
+                            compilation.assets[assetName] = {
+                                source: function () {
+                                    return processedCss;
+                                },
+                                size: function () {
+                                    return processedCss.length;
+                                }
+                            };
 
-                // Extract map back to assets
-                if (result.map) {
-                  const processedMap = result.map.toString();
+                            // Extract map back to assets
+                            if (result.map) {
+                                const processedMap = result.map.toString();
 
-                  compilation.assets[mapName] = {
-                    source: function () {
-                      return processedMap;
-                    },
-                    size: function () {
-                      return processedMap.length;
-                    },
-                  };
-                }
-              })
-              .catch(function (err) {
-                hasErrors = true;
-                throw new Error(
-                  "CSS minification error: " +
-                    err.message +
-                    ". File: " +
-                    assetName
-                );
-              });
-            promises.push(promise);
-          });
+                                compilation.assets[mapName] = {
+                                    source: function () {
+                                        return processedMap;
+                                    },
+                                    size: function () {
+                                        return processedMap.length;
+                                    }
+                                };
+                            }
+                        })
+                        .catch(function (err) {
+                            hasErrors = true;
+                            throw new Error('CSS minification error: ' + err.message + '. File: ' + assetName);
+                        });
+                    promises.push(promise);
+                });
 
-          Promise.all(promises)
-            .then(function () {
-              callback();
-            })
-            .catch(callback);
-        }
-      );
-    };
+                Promise.all(promises)
+                    .then(function () {
+                        callback();
+                    })
+                    .catch(callback);
+            });
+        };
 } catch (error) {
-  // vuecli 5.0,css文件中中不能目前不能引用绝对路径，需要写成内联样式才行
+    // vuecli 5.0,css文件中中不能目前不能引用绝对路径，需要写成内联样式才行
 }
--- dist/AddJsStaticDir.js
+++ dist/AddJsStaticDir.js
@@ -1,26 +1,26 @@
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = function (terserOptions = {}) {
-  return new TerserPlugin({
-    terserOptions: {
-      compress: {
-        drop_console: true, //console
-        drop_debugger: false,
-        pure_funcs: ["console.log"]
-      },
-      ...terserOptions
-    },
-    ...(TerserPlugin.terserMinify && TerserPlugin.terserMinify.getMinimizerVersion() > "4" ? {} : { cache: false }),
+    return new TerserPlugin({
+        terserOptions: {
+            compress: {
+                drop_console: true, //console
+                drop_debugger: false,
+                pure_funcs: ['console.log']
+            },
+            ...terserOptions
+        },
+        ...(TerserPlugin.terserMinify && TerserPlugin.terserMinify.getMinimizerVersion() > '4' ? {} : { cache: false }),
 
-    minify: async (file, sourceMap, minimizerOptions) => {
-      const extractedComments = [];
-      const { minify: terserMinify } = require("terser");
-      const result = await terserMinify(file, minimizerOptions);
-      const isProd = process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production";
-      if (isProd) {
-        let reg = new RegExp(process.env.RegExpStr, "g");
-        result.code = result.code.replace(reg, process.env.JsCssStaticReplaceDir);
-      }
-      return { ...result, extractedComments };
-    }
-  });
+        minify: async (file, sourceMap, minimizerOptions) => {
+            const extractedComments = [];
+            const { minify: terserMinify } = require('terser');
+            const result = await terserMinify(file, minimizerOptions);
+            const isProd = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production';
+            if (isProd) {
+                let reg = new RegExp(process.env.RegExpStr, 'g');
+                result.code = result.code.replace(reg, process.env.JsStaticReplaceDir);
+            }
+            return { ...result, extractedComments };
+        }
+    });
 };
